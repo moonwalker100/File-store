adaa8842737f10788470c6c5e3df2f8a6d51edcb
Moonwalker <moonwalker1092@gmail.com>
2025-08-11 19:47:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,12 +15,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 caption=f"""<blockquote expandable> <b>
 🤖 ᴍʏ ɴᴀᴍᴇ: PLAY BOT
 ╭───────────⍟
-├➽ Dᴇᴠᴇʟᴏᴩᴇʀ : ᴜɴᴋɴᴏᴡɴ 
-├➽ Lɪʙʀᴀʀy : Pʏʀᴏɢʀᴀᴍ
+├➽ Dᴇᴠᴇʟᴏᴩᴇʀ : </b> <a href='tg://user?id={}'>ᴜɴᴋɴᴏᴡɴ</a>",
+├➽ Lɪʙʀᴀʀy : </b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a>",
 ├➽ Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ 3
 ├➽ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ : ꜱᴏʟᴏ ᴄᴏᴅᴇ
-├➽ Mᴀɪɴ Cʜᴀɴɴᴇʟ : ᴛʜɪꜱ ᴄʜᴀɴɴᴇʟ 
-├➽ ᴏᴡɴᴇʀ : Bʟᴀᴄᴋ Wᴀʟᴋᴇʀ 🜲
+├➽ ᴏᴡɴᴇʀ : </b> <a href='tg://user?id={1718481517}'>Bʟᴀᴄᴋ Wᴀʟᴋᴇʀ 🜲</a>",
 ╰───────────────⍟
 </b></blockquote>"""
             ),
