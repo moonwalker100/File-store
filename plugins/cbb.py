0dab6fd4c58858026f7c28f6f4365f34e5126788
Moonwalker <moonwalker1092@gmail.com>
2025-08-11 14:48:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,25 +3,28 @@
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        await query.message.edit_text(
-            text = f"""<blockquote>
+        await query.message.edit_media(
+            media=InputMediaPhoto(
+                media="https://envs.sh/h7m.jpg",  # Replace with your photo URL or local file path
+                caption=f"""<blockquote expandable> <b>
 🤖 ᴍʏ ɴᴀᴍᴇ: PLAY BOT
 ◈ ᴏᴡɴᴇʀ: @Black_walker_id
 ◈ ʟᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3
 ◈ ʟɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ ᴠ2
 ◈ ᴅᴀᴛᴀʙᴀsᴇ: Mᴏɴɢᴏ ᴅʙ
-🧑‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: @searchingshiv </blockquote>""",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+🧑‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: @searchingshiv
+</b></blockquote>"""
+            ),
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🔒 Close", callback_data="close")
                     ]
                 ]
             )
